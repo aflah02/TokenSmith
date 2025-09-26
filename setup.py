8a9f5caa7579f6bfecdcf5a2a69429300af96f0a
Aflah <72096386+aflah02@users.noreply.github.com>
2025-09-26 13:10:37 +0200

    Update author details and repository URL in setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name='tokensmith',
     version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
     description='A package for managing datasets with editing, inspecting, sampling, exporting, and searching functionalities.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/tokensmith',  # Replace with your actual repository URL
+    url='https://github.com/aflah02/TokenSmith', 
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',  # Replace with your actual license
+        'License :: Apache 2.0 License', 
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.8',
