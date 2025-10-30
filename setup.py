3e6bc62cc1fbe2898a2b5b7051eeead92db9baf9
Mohammad Aflah Khan <aflah20082@iiitd.ac.in>
2025-10-30 14:50:40 +0100

    Fix Versioning of Dependencies

--- setup.py
+++ setup.py
@@ -1,42 +1,65 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tokensmith',
-    version='0.1.0',
-    description='A package for managing datasets with editing, inspecting, sampling, exporting, and searching functionalities.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/aflah02/TokenSmith', 
+    name="tokensmith",
+    version="0.1.1",
+    description=(
+        "A Package for Streamlining Data Editing, Search, and Inspection for "
+        "Large-Scale Language Model Training and Interpretability"
+    ),
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Mohammad Aflah Khan, Ameya Godbole",
+    author_email="afkhan@mpi-sws.org, ameyagod@usc.edu",
+    license="MIT",
+    url="https://github.com/aflah02/tokensmith",
+    project_urls={
+        "Homepage": "https://github.com/aflah02/tokensmith",
+        "Repository": "https://github.com/aflah02/tokensmith",
+    },
+    keywords=[
+        "dataset",
+        "management",
+        "editing",
+        "sampling",
+        "exporting",
+        "searching",
+    ],
     packages=find_packages(),
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: Apache 2.0 License', 
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires=="==3.11.2",
     install_requires=[
-        'numpy>=1.21.0',
-        'pandas>=1.3.0', 
-        'tqdm>=4.62.0',
+        "numpy>=1.26.4",
+        "pandas>=2.2.3",
+        "tqdm>=4.67.1",
     ],
     extras_require={
-        'ui': [
-            'streamlit>=1.20.0',
-            'altair>=4.2.0',
+        "docs": [
+            "mkdocs>=1.6.1",
+            "mkdocs-material>=9.6.14",
+            "mkdocstrings[python]>=0.29.1",
+            "mkdocstrings-python>=1.16.12",
+            "mkdocs-autorefs>=1.4.2",
+            "mkdocs-material-extensions>=1.3.1",
+            "mkdocs-get-deps>=0.2.0",
+            "mkdocs-jupyter>=0.25.1",
         ],
-        'search': [
-            'tokengrams>=0.3.0',
+        "ui": [
+            "streamlit>=1.46.0",
+            "altair>=5.5.0",
         ],
-        'docs': [
-            'mkdocs>=1.5.0',
-            'mkdocs-material>=9.0.0', 
-            'mkdocstrings[python]>=0.24.0',
-            'mkdocstrings-python>=1.7.0',
+        "search": [
+            "tokengrams>=0.3.3",
         ],
-        'all': [
-            'streamlit>=1.20.0',
-            'altair>=4.2.0',
-            'tokengrams>=0.3.0',
+        "all": [
+            "streamlit>=1.46.0",
+            "altair>=5.5.0",
+            "tokengrams>=0.3.3",
         ],
     },
+    include_package_data=True,
 )
